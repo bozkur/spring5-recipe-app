0a8d9451a409ffd6f314bc5bab7c8e93a6a08b95
Cevher <ccb@ccb.org>
2022-01-24 00:40:18 +0300

    Added bootstrap CSS.

--- src/main/java/guru/springframework/controller/IndexController.java
+++ src/main/java/guru/springframework/controller/IndexController.java
@@ -4,8 +4,10 @@ import guru.springframework.domain.Category;
 import guru.springframework.domain.UnitOfMeasure;
 import guru.springframework.repositiories.CategoryRepository;
 import guru.springframework.repositiories.UnitOfMeasureRepository;
+import guru.springframework.service.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
@@ -16,18 +18,22 @@ public class IndexController {
 
     private CategoryRepository categoryRepository;
     private UnitOfMeasureRepository unitOfMeasureRepository;
+    private RecipeService recipeService;
 
-    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository, RecipeService recipeService) {
         this.categoryRepository = categoryRepository;
         this.unitOfMeasureRepository = unitOfMeasureRepository;
+        this.recipeService = recipeService;
     }
 
     @RequestMapping("/")
-    public String index() {
+    public String index(Model model) {
         Optional<Category> category = categoryRepository.findByCategoryName("Mexican");
         Optional<UnitOfMeasure> uom = unitOfMeasureRepository.findByDescription("Pinch");
         log.info("Found category's id: {}", category.get().getId());
         log.info("Found unit of measure's id:{}", uom.get().getId());
+        model.addAttribute("recipes", recipeService.getRecipes());
         return "index";
     }
 }
