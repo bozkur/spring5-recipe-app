f505690c82c7ab833db13ce9f22e9734cbc0ff32
Cevher <ccb@ccb.org>
2022-07-05 06:00:35 +0300

    Github Token denemesi.
    Token çalışıyor mu diye kontrol edilecek.

--- src/main/java/guru/springframework/Spring5RecipeAppApplication.java
+++ src/main/java/guru/springframework/Spring5RecipeAppApplication.java
@@ -3,6 +3,9 @@ package guru.springframework;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Deneme
+ */
 @SpringBootApplication
 public class Spring5RecipeAppApplication {
 
